53556362c73066795b4fda5f630ccc6b77855f1c
heonjuun <s01077314486@gmail.com>
2026-02-25 09:04:58 +0900

    upperCase
    upper

--- main.cpp
+++ main.cpp
@@ -3,14 +3,14 @@ using namespace std;
 
 int main()
 {
-	int number1 = 0;
-	int number2 = 0;
-	cin >> number1;
-	cin >> number2;
-	cout << number1 + number2 << endl;
-	cout << number1 - number2 << endl;
-	cout << number1 * number2 << endl;
-	cout << number1 / number2 << endl;
-	cout << number1 % number2 << endl;
+	int Number1 = 0;
+	int Number2 = 0;
+	cin >> Number1;
+	cin >> Number2;
+	cout << Number1 + Number2 << endl;
+	cout << Number1 - Number2 << endl;
+	cout << Number1 * Number2 << endl;
+	cout << Number1 / Number2 << endl;
+	cout << Number1 % Number2 << endl;
 
 }
