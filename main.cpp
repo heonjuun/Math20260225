965a7324a694cba041c6b556b71905a32162a5d1
heonjuun <s01077314486@gmail.com>
2026-02-25 09:00:54 +0900

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int number1 = 0;
+	int number2 = 0;
+	cin >> number1;
+	cin >> number2;
+	cout << number1 + number2 << endl;
+	cout << number1 - number2 << endl;
+	cout << number1 * number2 << endl;
+	cout << number1 / number2 << endl;
+	cout << number1 % number2 << endl;
+
+}
